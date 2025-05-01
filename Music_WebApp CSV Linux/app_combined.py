45a4f7cfc8c4bba8d796a8d75918c0c29ee885ea
Milind Kumar <milindkr707@gmail.com>
2025-05-01 21:02:51 +0530

    Update app configuration for production: disable debug mode and set host and port

--- Music_WebApp CSV Linux/app_combined.py
+++ Music_WebApp CSV Linux/app_combined.py
@@ -123,4 +123,4 @@ def download_all_songs():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)
---
+++ Music_WebApp CSV Linux/config.py
@@ -0,0 +1,5 @@
+# Path to the folder where downloaded files will be stored
+DOWNLOAD_FOLDER = '/home/milind/Downloads/Music/'
+
+# Path to the ffmpeg binary
+FFMPEG_LOCATION = '/usr/bin/ffmpeg'
