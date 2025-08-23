8522f9174e5e0b8d7100c1811904b502ab82889c
Milind Kumar <milindkr707@gmail.com>
2025-08-23 18:28:03 +0530

    changed the folder name for more cleanliness

--- master_launcher.py
+++ master_launcher.py
@@ -4,9 +4,9 @@ import platform
 
 def main():
     if platform.system() == "Windows":
-        os.system("python Musicfy_Windows/app_combined.py")
+        os.system("python Windows/app_combined.py")
     elif platform.system() == "Linux":
-        os.system("python Musicfy_Linux/app_combined.py")
+        os.system("python Linux/app_combined.py")
     else:
         print("Unsupported operating system. Please run this script on Windows or Linux.")
 
