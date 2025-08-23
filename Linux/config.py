e73f0af97c64f694ff0e1920af77c8d22110982a
Milind Kumar <milindkr707@gmail.com>
2025-08-23 18:28:41 +0530

    enabled dynamic download folder fetching

--- Linux/config.py
+++ Linux/config.py
@@ -1,5 +1,9 @@
 # Path to the folder where downloaded files will be stored
-DOWNLOAD_FOLDER = '/home/milind/Downloads/Music/'
+from os import environ, path
+
+HOME = environ["HOME"]
+DOWNLOAD_FOLDER = path.join(HOME, 'Downloads/Music/')
 
 # Path to the ffmpeg binary
 FFMPEG_LOCATION = '/usr/bin/ffmpeg'
+
