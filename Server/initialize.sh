e6c0513d90feb07b398d66cbf8f6f45abe904cd6
Milind Kumar <milindkr707@gmail.com>
2025-10-06 00:16:39 +0530

    Add initialization and start scripts for environment setup and application launch for server

---
+++ Server/initialize.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Update package lists and install system dependencies
+ apt update 
+ apt install -y python3 python3-pip python3-venv git
+
+cd /data
+
+# Create a virtual environment
+python3 -m venv py_venv
+
+# Activate the virtual environment
+source py_venv/bin/activate
+
+# Install required Python packages
+pip install --upgrade pip
+pip install -r ../Linux/requirements.txt
+
+# Set up environment variables (if any)
+# export VAR_NAME=value
+
+# Print success message
+echo "Environment setup complete. Virtual environment is ready."
+
+deactivate
+
+# Copy start_app.sh to /data/
+cp Musicfy_py/Server/start_app.sh /data/
+
+# Start the application
+bash /data/start_app.sh
---
+++ Server/start_app.sh
@@ -0,0 +1,15 @@
+start_app.sh 
+#! /bin/bash
+
+
+cd /data/Musicfy_py/
+source /data/py_venv/bin/activate 
+
+echo "=-=-=-===============================-=-=" $(date) "=--=----------=============================================-=-=--=" >> logs/logging_musicfy.log
+echo "Starting app $(date)"
+nohup /data/py_venv/bin/python3 ./master_launcher.py >> logs/logging_musicfy.log &
+echo "=-=-=-===============================-=-=" $(date) "=--=----------=============================================-=-=--=" >> logs/housekeeping_logging.log
+echo "Starting Housekeeping $(date)"
+nohup /data/py_venv/bin/python3 ./housekeeping_linux.py >> logs/housekeeping_logging.log &
+
+
