0a4e227a171c7d8670e093ac4c43b5cc438242df
Milind Kumar <89257703+milindk20@users.noreply.github.com>
2025-12-10 23:59:42 +0530

    Add ffmpeg installation to initialize script

--- Server/initialize.sh
+++ Server/initialize.sh
@@ -2,7 +2,7 @@
 
 # Update package lists and install system dependencies
  apt update 
- apt install -y python3 python3-pip python3-venv git
+ apt install -y python3 python3-pip python3-venv git ffmpeg
 
 cd /data
 
